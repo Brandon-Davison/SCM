a77319efa13b13a990bf4bc7f4cec34fecbcf1b9
Brandon.Davison@wsu.edu <bdavison@eecs.wsu.edu>
2019-12-12 18:48:08 -0800

    Another component added in index

---
+++ src/MyComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MyComponent() {
+    return (
+        <div>
+            <h6>THIS is another compoennt</h6>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import MyComponent from './MyComponent'
+
+// class MyComponent extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <Button>My Button</Button>
+//       </div>
+//     )
+//   }
+// }
 
 const App = () => {
-  return <div>Hello React,Webpack 4 & Babel 7!</div>;
-};
+  return (
+    <div className="App">
+      <h1>My title</h1>
+      <h4>Another one</h4>
+      <MyComponent></MyComponent>
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
